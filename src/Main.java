bea640056b26e8a3f9075f1c9b08d7e289d2195a
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:57:24 +0100

    Cambio en rama1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola buenas!");
+
+        System.out.println("Me llamo Janire.");
+        for (int i=1; i<5; i++) {
+            System.out.println(i);
+        }
     }
 }
